fe61fc729176c39fec913f2cc6960b207add4318
Aditi Joshi <adititeena3458@gmail.com>
2025-09-30 21:44:13 +0530

    Update vite.config.js

--- Frontend/crackit/vite.config.js
+++ Frontend/crackit/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  preview: {
+    port: 4173, // optional, local testing port
+    host: true, // 0.0.0.0 pe listen kare
+    allowedHosts: [
+      'crackit-frontend2.onrender.com' // Render frontend domain
+    ]
+  }
 })
